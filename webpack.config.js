f5e7e3fc22184922afe10552f1bfe9e4c9c7a4f1
Dan Lucraft <dan@songkick.com>
2016-11-16 14:57:27 +0000

    Make webpack dev server work

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     },
     output: {
         path: __dirname + "/dist",
-        filename: "[name].bundle.js"
+        filename: "[name].bundle.js",
+        publicPath: "/dist/",
     }
 }
